a51aaea969e002d9780b0acebd4c2f9b5b6cd94e
肖朝霞 <zx.xiao@tianrang-inc.com>
2020-04-30 00:18:56 +0800

    feat:add eslint file

--- src/pages/Result/index.js
+++ src/pages/Result/index.js
@@ -1,32 +1,33 @@
-import React,{useContext} from 'react';
-import todoStore from '../../stores/index';
-import style from './index.module.less';
-import {Link} from 'react-router-dom';
-import {Button} from 'antd';
-import { inject,observer } from 'mobx-react';
+import React, { useContext } from "react";
+import todoStore from "../../stores/index";
+import style from "./index.module.less";
+import { Link } from "react-router-dom";
+import { Button } from "antd";
+import { inject, observer } from "mobx-react";
 
-@inject('todoStore')
+@inject("todoStore")
 @observer
 class ResultC extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     // console.log('props',this.props);
   }
 
   render() {
-    const {todoStore:{ hasDone}} = this.props;
+    const {
+      todoStore: { hasDone },
+    } = this.props;
 
     return (
       <div className={style.result}>
-      <h3 className={style.info}>
-       完成计划数为:{" "}
-        <span className={style.alreadyDone}>{hasDone}</span>
-      </h3>
-      <Link to="/todo">
-        <Button type="primary">返回todoList</Button>
-      </Link>
-        </div>
-    )
+        <h3 className={style.info}>
+          完成计划数为: <span className={style.alreadyDone}>{hasDone}</span>
+        </h3>
+        <Link to="/todo">
+          <Button type="primary">返回todoList</Button>
+        </Link>
+      </div>
+    );
   }
 }
 
--- src/pages/Result/index.tsx
+++ src/pages/Result/index.tsx
@@ -1,26 +1,24 @@
-import React,{useContext} from 'react';
-import todoStore from '../../stores/index';
-import style from './index.module.less';
-import {Link} from 'react-router-dom';
-import {Button} from 'antd';
+import React, { useContext } from "react";
+import todoStore from "../../stores/index";
+import style from "./index.module.less";
+import { Link } from "react-router-dom";
+import { Button } from "antd";
 import ToDoContext from "../../stores/index";
 
 const Result = () => {
   const store = useContext(ToDoContext);
-  const {hasDone} = store;
+  const { hasDone } = store;
 
   return (
     <div className={style.result}>
- 
       <h3 className={style.info}>
-       完成计划数为:{" "}
-        <span className={style.alreadyDone}>{hasDone}</span>
+        完成计划数为: <span className={style.alreadyDone}>{hasDone}</span>
       </h3>
       <Link to="/todo">
         <Button type="primary">返回todoList</Button>
       </Link>
-        </div>
-  )
-}
+    </div>
+  );
+};
 
 export default Result;
