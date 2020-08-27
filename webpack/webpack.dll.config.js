5e68de2de43c15a77fe0fcb8c8390b5bb57605a5
肖朝霞 <zx.xiao@tianrang-inc.com>
2020-08-27 15:02:29 +0800

    feat:add

--- webpack/webpack.dll.config.js
+++ webpack/webpack.dll.config.js
@@ -1,22 +1,23 @@
-const webpack = require('webpack');
-const path = require('path');
-const paths = require('./config/paths');
+const webpack = require('webpack')
+const path = require('path')
+const paths = require('./config/paths')
 
-module.exports ={
+module.exports = {
   entry: {
-    react:['react'],
+    react: ['react'],
     reactDOM: ['react-dom'],
   },
   output: {
     path: paths.appDll,
     filename: '[name].todo.dll.js',
-    library: '_dll_[name]_library',
+    library: '_dll_[name]_library', // 存放动态链接库的全局变量名称，例如对应react来说就是——dll_react
+    // 之所以在前面加上_dll_是为了防止全局变量冲突
   },
   plugins: [
     new webpack.DllPlugin({
       context: paths.appRoot,
       name: '_dll_[name]_library', //定义打包的公共文件对外暴露的函数名
-      path: path.resolve(paths.appDll, '[name].todo.manifest.json')  // manifest.json 描述动态链接库包含了哪些内容
-    })
-  ]
+      path: path.resolve(paths.appDll, '[name].todo.manifest.json'), // manifest.json 描述动态链接库包含了哪些内容
+    }),
+  ],
 }
